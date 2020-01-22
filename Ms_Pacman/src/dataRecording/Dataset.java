c530150b0c7e491bbd993d8a68681242cf855849
Mzet <martindahren@gmail.com>
2020-01-22 16:48:35 +0100

    Added code to Dataset and ID3

--- Ms_Pacman/src/dataRecording/Dataset.java
+++ Ms_Pacman/src/dataRecording/Dataset.java
@@ -5,17 +5,55 @@ import java.util.HashMap;
 
 // TODO
 public class Dataset {
-	private DataTuple[] dataset;
+	//private DataTuple[] dataset;
+	public ArrayList<DataTuple> dataset;
+	public HashMap<String, HashMap<String, Integer>> attributesWithValuesAndCounts;
 	
 	
-	public Dataset(DataTuple[] dataTuples) {
+	public Dataset(ArrayList<DataTuple> dataTuples) {
 		dataset = dataTuples;
+		attributesWithValuesAndCounts = new HashMap<>();
+		fillAttributesHashMap();
 
 	}
 	
-	public DataTuple[] getTuples() {
+	public Dataset getSubDataSetWithValue(String attribute, String value) {
+		ArrayList<DataTuple> newDataset = new ArrayList<DataTuple>();
+		
+		for(DataTuple tuple : dataset) {
+			if(tuple.discretizeAttrValue(attribute).equals(value)) {
+				newDataset.add(tuple);
+			}
+		}
+		return new Dataset(newDataset);
+	}
+	
+	/*public DataTuple[] getTuples() {
+		return dataset;
+	}*/
+	
+	public ArrayList<DataTuple> getTuples(){
 		return dataset;
 	}
 	
+	public void fillAttributesHashMap() {
+		for (DataTuple t : this.dataset) {
+			for (String attr: t.getHashMap().keySet()) {
+				String value = t.getHashMap().get(attr);
+				if(attributesWithValuesAndCounts.containsKey(attr)) {
+					if(attributesWithValuesAndCounts.get(attr).containsKey(value)) {
+						Integer count = attributesWithValuesAndCounts.get(attr).get(value);
+						count++;
+						attributesWithValuesAndCounts.get(attr).put(value, count);
+					} else {
+						attributesWithValuesAndCounts.get(attr).put(value, 1);
+					}
+				} else {
+					attributesWithValuesAndCounts.put(attr, new HashMap<String, Integer>());
+					attributesWithValuesAndCounts.get(attr).put(value, 1);
+				}
+			}
+		}
+	}
 
 }
--- Ms_Pacman/src/decisionTree/ID3.java
+++ Ms_Pacman/src/decisionTree/ID3.java
@@ -1,19 +1,88 @@
 package decisionTree;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import dataRecording.Dataset;
 
 public class ID3 {
 	
 	public ID3(Dataset dataset, ArrayList<String> listOfAttributes, String label, Node node) {
-		
+
 	}
 	
-	
-	
-	public void calculateEntropy() {
+	public String attributeSelection(Dataset dataset, List<String> attributeList) {
+		String res = null;
+		float infoD = calculateEntropy(dataset, "strategy");
 		
+		ArrayList<Float> gainA = new ArrayList<Float>();
+		
+		for(String attr : attributeList) {
+			float infoADAux = infoAD(dataset, attr);
+			float gain = infoD - infoADAux;
+			gainA.add(gain);
+		}
+		
+		float max = 0;
+		int posMax = 0;
+		
+		for(int i = 0; i < gainA.size(); i++) {
+			if(gainA.get(i) > max) {
+				max = gainA.get(i);
+				posMax = i;
+			}
+		}
+		
+		res = attributeList.get(posMax);
+		return res;
+	}
+	
+	public float infoAD(Dataset dataset, String attribute) {
+		float info = 0;
+		
+		HashMap<String, Integer> map = dataset.attributesWithValuesAndCounts.get(attribute);
+		List<String> values = new ArrayList(map.keySet());
+		for(String value : values) {
+			Dataset dt = dataset.getSubDataSetWithValue(attribute, value);
+			float sizeDataset = dataset.dataset.size();
+			float subDataset = dt.dataset.size();
+			info += (subDataset / sizeDataset) * calculateEntropy(dt, "strategy");
+		}
+		return info;
+	}
+	
+	public float calculateEntropy(Dataset dataset, String attribute) {
+		float infoD = 0;
+		
+		HashMap<String, Integer> map = dataset.attributesWithValuesAndCounts.get(attribute);
+		
+		List<String> strategyValues = new ArrayList(map.keySet());
+		
+		for(String strategyVal : strategyValues) {
+			float pi = calculateProbability(dataset, attribute, strategyVal);
+			if(pi > 0) {
+				float res = -pi * log2(pi);
+				
+				infoD += res;
+			} else if(pi == (-1)) {
+				return infoD;
+			}
+		}
+		return infoD;
+	}
+	
+	public float calculateProbability(Dataset dataset, String attributes, String value) {
+		HashMap<String, Integer> mapValuesAttr = dataset.attributesWithValuesAndCounts.get(attributes);
+		
+		float sizeAttr = mapValuesAttr.get(value);
+		float sizeDataset = dataset.dataset.size();
+		
+		return sizeAttr / sizeDataset; 
+	}
+	
+	float log2(float x) {
+		return (float) (Math.log(x) / Math.log(2));
 	}
 	
 	public void calculateAverageInfo() {
