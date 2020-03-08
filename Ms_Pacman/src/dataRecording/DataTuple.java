f101c4562868ce380648c1994c8aed6f7e5bb36d
Mzet <martindahren@gmail.com>
2020-03-08 16:17:48 +0100

    Updated dataset and datatuple files.

--- Ms_Pacman/src/dataRecording/DataTuple.java
+++ Ms_Pacman/src/dataRecording/DataTuple.java
@@ -1,13 +1,12 @@
 package dataRecording;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 import pacman.game.Constants;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
+import pacman.game.Constants.STRATEGY;
 import pacman.game.Game;
 
 public class DataTuple {
@@ -32,12 +31,9 @@ public class DataTuple {
 	// ====================
 	// ADDED BY JQ
 	//
-
+	public int closestPillDist = -1;
+	public int closestPowerPillDist = -1;
 	public Constants.STRATEGY strategy;
-	public int closestGhostDist;
-	public int closestEdibleGhostDist;
-	public int closestPillDist;
-	public int closestPowerPillDist;
 
 	public HashMap<String, String> map; // <attribute, attributeValue>
 
@@ -84,6 +80,7 @@ public class DataTuple {
 		}
 
 		strategy = game.strategy;
+		directionChosen = move;
 
 		this.mazeIndex = game.getMazeIndex();
 		this.currentLevel = game.getCurrentLevel();
@@ -94,6 +91,9 @@ public class DataTuple {
 		this.currentLevelTime = game.getCurrentLevelTime();
 		this.numOfPillsLeft = game.getNumberOfActivePills();
 		this.numOfPowerPillsLeft = game.getNumberOfActivePowerPills();
+		
+		this.closestPillDist = getClosestPillOrPowerPillDist(game, 0);
+		this.closestPowerPillDist = getClosestPillOrPowerPillDist(game, 1);
 
 		if (game.getGhostLairTime(GHOST.BLINKY) == 0) {
 			this.isBlinkyEdible = game.isGhostEdible(GHOST.BLINKY);
@@ -132,32 +132,7 @@ public class DataTuple {
 		this.numberOfTotalPillsInLevel = game.getNumberOfPills();
 		this.numberOfTotalPowerPillsInLevel = game.getNumberOfPowerPills();
 
-		int closestGhostDist = Collections.min(Arrays.asList(blinkyDist, inkyDist, pinkyDist, sueDist));
-
-		// set the closest edible ghost
-		if (closestGhostDist == blinkyDist && isBlinkyEdible) {
-			closestEdibleGhostDist = blinkyDist;
-		} else if (closestGhostDist == inkyDist && isInkyEdible) {
-			closestEdibleGhostDist = inkyDist;
-		} else if (closestGhostDist == pinkyDist && isPinkyEdible) {
-			closestEdibleGhostDist = pinkyDist;
-		} else if (closestGhostDist == sueDist && isSueEdible) {
-			closestEdibleGhostDist = sueDist;
-		}
-
-		// set the closest non-edible ghost
-		if (closestGhostDist == blinkyDist && !isBlinkyEdible) {
-			this.closestGhostDist = blinkyDist;
-		} else if (closestGhostDist == inkyDist && !isInkyEdible) {
-			this.closestGhostDist = inkyDist;
-		} else if (closestGhostDist == pinkyDist && !isPinkyEdible) {
-			this.closestGhostDist = pinkyDist;
-		} else if (closestGhostDist == sueDist && !isSueEdible) {
-			this.closestGhostDist = sueDist;
-		}
-
-		closestPowerPillDist = Arrays.stream(game.getPowerPillIndices()).min().getAsInt(); // ignore nommed p-pills?
-		closestPillDist = Arrays.stream(game.getPillIndices()).min().getAsInt(); // ignore nommed pills?;
+		
 	}
 
 	public DataTuple(String data) {
@@ -165,82 +140,60 @@ public class DataTuple {
 
 		strategy = Constants.STRATEGY.valueOf(dataSplit[0]);
 
-		this.directionChosen = MOVE.valueOf(dataSplit[1]);
-
-		this.pacmanPosition = Integer.parseInt(dataSplit[2]);
+		this.closestPillDist =Integer.parseInt(dataSplit[1]);
+		this.closestPowerPillDist =Integer.parseInt(dataSplit[2]);
 		this.numOfPillsLeft = Integer.parseInt(dataSplit[3]);
 		this.numOfPowerPillsLeft = Integer.parseInt(dataSplit[4]);
-		this.blinkyDir = MOVE.valueOf(dataSplit[5]);
-		this.inkyDir = MOVE.valueOf(dataSplit[6]);
-		this.pinkyDir = MOVE.valueOf(dataSplit[7]);
-		this.sueDir = MOVE.valueOf(dataSplit[8]);
-		closestPowerPillDist = Integer.parseInt(dataSplit[9]);
-		closestPillDist = Integer.parseInt(dataSplit[10]);
-		closestGhostDist = Integer.parseInt(dataSplit[11]);
-		closestEdibleGhostDist = Integer.parseInt(dataSplit[12]);
+		this.inkyDist = Integer.parseInt(dataSplit[5]);
+		this.pinkyDist = Integer.parseInt(dataSplit[6]);
+		this.sueDist = Integer.parseInt(dataSplit[7]);
+		this.blinkyDist = Integer.parseInt(dataSplit[8]);
+		this.isInkyEdible = Boolean.parseBoolean(dataSplit[9]);
+		this.isPinkyEdible = Boolean.parseBoolean(dataSplit[10]);
+		this.isSueEdible = Boolean.parseBoolean(dataSplit[11]);
+		this.isBlinkyEdible = Boolean.parseBoolean(dataSplit[12]);
 
-		// this.DirectionChosen = MOVE.valueOf(dataSplit[0]);
-		//
-		// this.mazeIndex = Integer.parseInt(dataSplit[1]);
-		// this.currentLevel = Integer.parseInt(dataSplit[2]); // remove
-		// this.pacmanPosition = Integer.parseInt(dataSplit[3]);
-		// this.pacmanLivesLeft = Integer.parseInt(dataSplit[4]);
-		// this.currentScore = Integer.parseInt(dataSplit[5]); // remove
-		// this.totalGameTime = Integer.parseInt(dataSplit[6]); // remove
-		// this.currentLevelTime = Integer.parseInt(dataSplit[7]); // remove
-		// this.numOfPillsLeft = Integer.parseInt(dataSplit[8]);
-		// this.numOfPowerPillsLeft = Integer.parseInt(dataSplit[9]);
-		// this.isBlinkyEdible = Boolean.parseBoolean(dataSplit[10]);
-		// this.isInkyEdible = Boolean.parseBoolean(dataSplit[11]);
-		// this.isPinkyEdible = Boolean.parseBoolean(dataSplit[12]);
-		// this.isSueEdible = Boolean.parseBoolean(dataSplit[13]);
-		// this.blinkyDist = Integer.parseInt(dataSplit[14]);
-		// this.inkyDist = Integer.parseInt(dataSplit[15]);
-		// this.pinkyDist = Integer.parseInt(dataSplit[16]);
-		// this.sueDist = Integer.parseInt(dataSplit[17]);
-		// this.blinkyDir = MOVE.valueOf(dataSplit[18]);
-		// this.inkyDir = MOVE.valueOf(dataSplit[19]);
-		// this.pinkyDir = MOVE.valueOf(dataSplit[20]);
-		// this.sueDir = MOVE.valueOf(dataSplit[21]);
-		// this.numberOfNodesInLevel = Integer.parseInt(dataSplit[22]);
-		// this.numberOfTotalPillsInLevel = Integer.parseInt(dataSplit[23]);
-		// this.numberOfTotalPowerPillsInLevel = Integer.parseInt(dataSplit[24]);
 	}
 
 	public String getSaveString() {
 		StringBuilder stringbuilder = new StringBuilder();
 
 		stringbuilder.append(strategy + ";");
-
-		stringbuilder.append(this.directionChosen + ";");
-		// stringbuilder.append(this.mazeIndex + ";");
-		// stringbuilder.append(this.currentLevel + ";");
-		stringbuilder.append(this.pacmanPosition + ";");
-		// stringbuilder.append(this.pacmanLivesLeft + ";");
-		// stringbuilder.append(this.currentScore + ";");
-		// stringbuilder.append(this.totalGameTime + ";");
-		// stringbuilder.append(this.currentLevelTime + ";");
-		stringbuilder.append(this.numOfPillsLeft + ";");
-		stringbuilder.append(this.numOfPowerPillsLeft + ";");
-		// stringbuilder.append(this.isBlinkyEdible + ";");
-		// stringbuilder.append(this.isInkyEdible + ";");
-		// stringbuilder.append(this.isPinkyEdible + ";");
-		// stringbuilder.append(this.isSueEdible + ";");
-		// stringbuilder.append(this.blinkyDist + ";");
-		// stringbuilder.append(this.inkyDist + ";");
-		// stringbuilder.append(this.pinkyDist + ";");
-		// stringbuilder.append(this.sueDist + ";");
-		stringbuilder.append(this.blinkyDir + ";");
-		stringbuilder.append(this.inkyDir + ";");
-		stringbuilder.append(this.pinkyDir + ";");
-		stringbuilder.append(this.sueDir + ";");
-		// stringbuilder.append(this.numberOfNodesInLevel + ";");
-		// stringbuilder.append(this.numberOfTotalPillsInLevel + ";");
-		// stringbuilder.append(this.numberOfTotalPowerPillsInLevel + ";");
-		stringbuilder.append(closestGhostDist + ";");
-		stringbuilder.append(closestEdibleGhostDist + ";");
+		stringbuilder.append(closestPillDist + ";");
+		stringbuilder.append(closestPowerPillDist + ";");
+		stringbuilder.append(numOfPillsLeft + ";");
+		stringbuilder.append(numOfPowerPillsLeft + ";");
+		stringbuilder.append(inkyDist + ";");
+		stringbuilder.append(pinkyDist + ";");
+		stringbuilder.append(blinkyDist + ";");
+		stringbuilder.append(sueDist + ";");
+		stringbuilder.append(isBlinkyEdible + ";");
+		stringbuilder.append(isInkyEdible + ";");
+		stringbuilder.append(isPinkyEdible + ";");
+		stringbuilder.append(isSueEdible + ";");
+		
 
 		return stringbuilder.toString();
+	}
+
+	private int getClosestPillOrPowerPillDist(Game game, int pillType) {
+		int[] indexArr;
+		boolean stillAvailable = false;
+		if (pillType == 0) { // normal pill
+			indexArr = game.getActivePillsIndices();
+		} else { // power pill
+			indexArr = game.getActivePowerPillsIndices();
+		}
+		if (indexArr.length == 0) {
+			return -1;
+		}
+		int[] temp = new int[indexArr.length];
+
+		for (int i = 0; i < indexArr.length; i++) {
+			
+			temp[i] = game.getShortestPathDistance(game.getPacmanCurrentNodeIndex(), indexArr[i]);
+		}
+		return Arrays.stream(temp).min().getAsInt();
 	}
 
 	/**
@@ -274,6 +227,10 @@ public class DataTuple {
 	public DiscreteTag discretizePosition(int pos) {
 		double aux = this.normalizePosition(pos);
 		return DiscreteTag.DiscretizeDouble(aux);
+	}
+	
+	public String discretizeBoolean(boolean bool) {
+		return bool ? "true" : "false";
 	}
 
 	public double normalizeBoolean(boolean bool) {
@@ -339,44 +296,61 @@ public class DataTuple {
 
 	/**
 	 * Discretize the given attribute value and returns it as a string.
-	 * @param attrValue the attribute value to discretize
+	 * 
+	 * @param attrValue
+	 *            the attribute value to discretize
 	 * @return a string of the discretized attribute value or null as default.
 	 */
 	public String discretizeAttrValue(String attrValue) {
 		switch (attrValue) {
 		case "strategy":
 			return strategy.toString();
-		case "closestGhostDist":
-			return discretizeDistance(closestGhostDist).toString();
-		case "closestEdibleGhostDist":
-			return discretizeDistance(closestEdibleGhostDist).toString();
-		case "closestPowerPillDist":
-			return discretizeDistance(closestPowerPillDist).toString();
+		case "numOfPillsLeft":
+			return discretizeNumberOfPills(numOfPillsLeft).toString();
+		case "numOfPowerPillsLeft":
+			return discretizeNumberOfPowerPills(numOfPowerPillsLeft).toString();
 		case "closestPillDist":
 			return discretizeDistance(closestPillDist).toString();
-		case "numOfPillsLeft" : 
-			return discretizeNumberOfPills(numOfPillsLeft).toString();
-		case "numOfPowerPillsLeft" : 
-			return discretizeNumberOfPowerPills(numOfPowerPillsLeft).toString();
-			
+		case "closestPowerPillDist":
+			return discretizeDistance(closestPowerPillDist).toString();
+		 case "isBlinkyEdible":
+             return discretizeBoolean(this.isBlinkyEdible);
+         case "isInkyEdible":
+             return discretizeBoolean(this.isInkyEdible);
+         case "isPinkyEdible":
+             return discretizeBoolean(this.isPinkyEdible);
+         case "isSueEdible":
+             return discretizeBoolean(this.isSueEdible);
+         case "blinkyDist":
+             return discretizeDistance(this.blinkyDist).toString();
+         case "inkyDist":
+             return discretizeDistance(this.inkyDist).toString();
+         case "pinkyDist":
+             return discretizeDistance(this.pinkyDist).toString();
+         case "sueDist":
+             return discretizeDistance(this.sueDist).toString();
 		default:
 			return null;
 		}
 
 	}
 
+	
+	
+
 	/**
-	 * Creates a hashmap and fills it if it does not exist, else returns the hashmap.
+	 * Creates a hashmap and fills it if it does not exist, else returns the
+	 * hashmap.
+	 * 
 	 * @return the hashmap
 	 */
 	public HashMap<String, String> getHashMap() {
 		if (map == null) {
 			map = new HashMap<>();
 			// make sure attribute names are correct
-			String[] strArr = { "strategy", "closestGhostDist", "closestEdibleGhostDist", "closestPowerPillDist",
-					"closestPillDist", "inkyDir", "pinkyDir", "blinkyDir", "sueDir", "directionChosen",
-					"pacmanPosition", "numOfPillsLeft", "numOfPowerPillsLeft" };
-
+			String[] strArr = { "strategy", "closestPowerPillDist",
+					"closestPillDist", "numOfPillsLeft", "numOfPowerPillsLeft", "isBlinkyEdible", 
+					"isInkyEdible", "isPinkyEdible", "isSueEdible", "blinkyDist", "inkyDist", "pinkyDist", "sueDist"};
 			for (String str : strArr) {
 				map.put(str, discretizeAttrValue(str));
 			}
--- Ms_Pacman/src/dataRecording/Dataset.java
+++ Ms_Pacman/src/dataRecording/Dataset.java
@@ -5,55 +5,66 @@ import java.util.HashMap;
 
 // TODO
 public class Dataset {
-	//private DataTuple[] dataset;
-	public ArrayList<DataTuple> dataset;
-	public HashMap<String, HashMap<String, Integer>> attributesWithValuesAndCounts;
+	public DataTuple[] dataset;
+	public HashMap<String, HashMap<String, Integer>> attributes; // attribute, name of attribute value, amount of times 
 	
-	
-	public Dataset(ArrayList<DataTuple> dataTuples) {
+	public Dataset(DataTuple[] dataTuples) {
 		dataset = dataTuples;
-		attributesWithValuesAndCounts = new HashMap<>();
-		fillAttributesHashMap();
-
+		attributes = new HashMap<>();
+		fillMap();
 	}
 	
-	public Dataset getSubDataSetWithValue(String attribute, String value) {
-		ArrayList<DataTuple> newDataset = new ArrayList<DataTuple>();
+	/**
+	 * Fills the dataset's map with name of attribute, the attribute's values and 
+	 * how many times the values have appeared in the data in each datatuple
+	 */
+	private void fillMap() {
 		
-		for(DataTuple tuple : dataset) {
-			if(tuple.discretizeAttrValue(attribute).equals(value)) {
-				newDataset.add(tuple);
-			}
-		}
-		return new Dataset(newDataset);
-	}
-	
-	/*public DataTuple[] getTuples() {
-		return dataset;
-	}*/
-	
-	public ArrayList<DataTuple> getTuples(){
-		return dataset;
-	}
-	
-	public void fillAttributesHashMap() {
-		for (DataTuple t : this.dataset) {
-			for (String attr: t.getHashMap().keySet()) {
-				String value = t.getHashMap().get(attr);
-				if(attributesWithValuesAndCounts.containsKey(attr)) {
-					if(attributesWithValuesAndCounts.get(attr).containsKey(value)) {
-						Integer count = attributesWithValuesAndCounts.get(attr).get(value);
+		for (DataTuple tuple : dataset) {
+			
+			for (String attribute : tuple.getHashMap().keySet()) {
+				
+				String attrValue = tuple.getHashMap().get(attribute);
+				// check if the attribute already exist in dataset map 
+				if(attributes.containsKey(attrValue)) {
+					if(attributes.get(attribute).containsKey(attrValue)) {
+						int count = attributes.get(attribute).get(attrValue);
 						count++;
-						attributesWithValuesAndCounts.get(attr).put(value, count);
+						attributes.get(attribute).put(attrValue, count);
 					} else {
-						attributesWithValuesAndCounts.get(attr).put(value, 1);
+						attributes.get(attribute).put(attrValue, 1);
 					}
+					
 				} else {
-					attributesWithValuesAndCounts.put(attr, new HashMap<String, Integer>());
-					attributesWithValuesAndCounts.get(attr).put(value, 1);
+					// adds the attribute to the dataset map
+					attributes.put(attribute, new HashMap<String, Integer>());
+					attributes.get(attribute).put(attrValue, 1);
 				}
 			}
 		}
+		
 	}
+
+	public DataTuple[] getTuples() {
+		return dataset;
+	}
+
+	/**
+	 * Creates a sub-set of data containing the attribute's values.
+	 * @param attribute
+	 * @param value
+	 * @return
+	 */
+	public Dataset getSubset(String attribute, String value) {
+		ArrayList<DataTuple> newSet = new ArrayList<>();
+		for (DataTuple dataTuple : dataset) {
+			if(dataTuple.discretizeAttrValue(attribute).equals(value)) {
+				newSet.add(dataTuple);
+			}
+		}
+		DataTuple[] arr = (DataTuple[]) newSet.toArray();
+		return new Dataset(arr);
+	}
+	
 
 }
